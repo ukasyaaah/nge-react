d2512ee7b3e87f65385bd600e6db694ed441637c
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-08-13 19:19:07 +0700

    event listener

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,10 +1,37 @@
 import Button from "../../ui/Button";
 
+const makanan = [
+  {
+    nama: "Nasi Goreng",
+    harga: 10000,
+  },
+  {
+    nama: "Nasi Kebuli",
+    harga: 10000,
+  },
+];
+
 const Home = () => {
+  const showButton = false;
   return (
     <main>
-      <h1>Hello Ukhasyah's World</h1>
-      <Button type="button">Test</Button>
+      <h1 onClick={() => alert("Hello User!")}>Hello Ukhasyah's World</h1>
+
+      {/* Rendering Condition */}
+      {showButton ? (
+        <Button type="button">Ukhasyah</Button>
+      ) : (
+        <Button type="button">Test</Button>
+      )}
+      <br />
+      <br />
+
+      {/* Rendering List */}
+      {makanan.map((item, index) => (
+        <Button key={`makanan-${index}`} type="button">
+          {item.nama}
+        </Button>
+      ))}
     </main>
   );
 };
--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -3,10 +3,11 @@ import type { ReactNode } from "react";
 interface PropTypes {
   children: ReactNode;
   type?: "submit" | "button";
+  onClick?: () => void;
 }
 const Button = (props: PropTypes) => {
   const { children, type } = props;
-  return <button type={type}>{children}</button>;
+  return <button  type={type}>{children}</button>;
 };
 
 export default Button;
